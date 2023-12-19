e5f8b8acec9ba5bf6dcabb667fe394372b8e0544
TzKarmelo <tzrtzr@hotmail.com>
2023-12-19 11:20:34 +0100

    Subida Ejercicios

--- 1_Java_Basico/Semana_1/Dia_1/Ejercicio_4/src/ejercicio_4/Main.java
+++ 1_Java_Basico/Semana_1/Dia_1/Ejercicio_4/src/ejercicio_4/Main.java
@@ -20,15 +20,20 @@ public class Main {
         
         int operador2 = sc.nextInt();
         
+        int suma = operador1 + operador2;
+        int resta = operador1 - operador2;
+        int multiplicacion = operador1 * operador2;
+        double division = operador1 / operador2;
+        
         // Mostramos los resultados
         
-        System.out.println("\nLa suma de " + operador1 + " y "+ operador2 + " es: " + (operador1+operador2));
-        System.out.println("La resta de " + operador1 + " y " + operador2 + " es: " + (operador1-operador2));
-        System.out.println("La multiplicación de " + operador1 + " y " + operador2 +" es: " +(operador1*operador2));
+        System.out.println("\nLa suma de " + operador1 + " y "+ operador2 + " es: " + suma);
+        System.out.println("La resta de " + operador1 + " y " + operador2 + " es: " + resta);
+        System.out.println("La multiplicación de " + operador1 + " y " + operador2 +" es: " + multiplicacion);
         
-        // La división no da decimales pues són números enteros y si divides por 0 lanza una excepción By Zero...
+        // Si divides por 0 lanza una excepción By Zero...
         
-        System.out.println("La división de " + operador1 + " y " + operador2 + " es: " + (operador1/operador2) + "\n");
+        System.out.println("La división de " + operador1 + " y " + operador2 + " es: " + division + "\n");
                 
     }
     
