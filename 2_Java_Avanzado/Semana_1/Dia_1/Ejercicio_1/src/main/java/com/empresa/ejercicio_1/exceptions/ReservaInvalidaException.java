65cb1bcace833e4fdb9f841de2e34ac501d46516
TzKarmelo <tzrtzr@hotmail.com>
2024-01-02 16:05:23 +0100

    Modificación ejercicios

--- 2_Java_Avanzado/Semana_1/Dia_1/Ejercicio_1/src/main/java/com/empresa/ejercicio_1/exceptions/ReservaInvalidaException.java
+++ 2_Java_Avanzado/Semana_1/Dia_1/Ejercicio_1/src/main/java/com/empresa/ejercicio_1/exceptions/ReservaInvalidaException.java
@@ -4,21 +4,9 @@ import java.time.LocalDate;
 
 public class ReservaInvalidaException extends RuntimeException{
     
-    private String nombre;
-    private String destino;
-    private LocalDate fecha_viaje;
-    private int asientos;
-
+   
     public ReservaInvalidaException(String nombre, String destino, LocalDate fecha_viaje, int asientos) {
         super(String.format("\n\nSr. %s, el vuelo con destino %s el dia %s, no tiene %s asientos disponibles.\n", nombre, destino, fecha_viaje, asientos));
-        this.nombre = nombre;
-        this.destino = destino;
-        this.fecha_viaje = fecha_viaje;
-        this.asientos = asientos;
+        
     }
-
-    
-    
-    
-    
 }
