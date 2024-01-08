715835c867a0e70c85211aa251fe3c835e99446e
TzKarmelo <tzrtzr@hotmail.com>
2024-01-08 13:04:40 +0100

    Modificacion ejercicio

--- 2_Java_Avanzado/Semana_2/Dia_2/Ejercicio_2/src/main/java/com/mycompany/ejercicio_2/models/Mascota.java
+++ 2_Java_Avanzado/Semana_2/Dia_2/Ejercicio_2/src/main/java/com/mycompany/ejercicio_2/models/Mascota.java
@@ -13,21 +13,39 @@ public class Mascota<T> {
         this.especie = especie;
     }
 
-    public T getId() {
-        return id;
-    }
-
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public int getEdad() {
         return edad;
     }
 
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
     public String getEspecie() {
         return especie;
     }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    
 
     @Override
     public String toString() {
